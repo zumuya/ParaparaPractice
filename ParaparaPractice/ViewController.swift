d54cc72f596bee35077db38f82dbff09cbe198ea
zumuya <zumuya@gmail.com>
2020-07-10 17:38:16 +0900

    Simplified bindings.

--- ParaparaPractice/ViewController.swift
+++ ParaparaPractice/ViewController.swift
@@ -11,7 +11,8 @@ import Cocoa
 class ViewController: NSViewController
 {
 	@IBOutlet var paraparaView: ParaparaView!
-
+	
+	var observings: [Any] = []
 	override func viewDidLoad()
 	{
 		super.viewDidLoad()
@@ -19,20 +20,16 @@ class ViewController: NSViewController
 		paraparaView.images = (0..<5)
 			.map { NSImage(named: "cat\($0)")! }
 		
-		paraparaView.bind(.init(#keyPath(ParaparaView.speed)), to: self, withKeyPath: #keyPath(speed), options: nil)
-	}
-	deinit
-	{
-		paraparaView.unbind(.init(#keyPath(ParaparaView.speed)))
+		observings += [
+			bindParaparaView(\.speed, to: \.speed),
+		]
 	}
 	
 	//MARK: - Speed
 	
 	@objc dynamic var speed: CGFloat = 1.0
 	
-	@objc class var keyPathsForValuesAffectingIsSpeedDefault: Set<String> { [
-		#keyPath(speed)
-	] }
+	@objc class var keyPathsForValuesAffectingIsSpeedDefault: Set<String> { [#keyPath(speed)] }
 	@objc dynamic var isSpeedDefault: Bool { (speed == 1.0) }
 	
 	//MARK: - Actions
@@ -40,5 +37,28 @@ class ViewController: NSViewController
 	@IBAction func resetSpeed(_ sender: Any?)
 	{
 		speed = 1.0
+	}
+	
+	//MARK: - Parapara View Binding
+	
+	func bindParaparaView<Value>(_ paraparaKeyPath: WritableKeyPath<ParaparaView, Value>, to keyPath: KeyPath<ViewController, Value>) -> Any
+	{
+		return observe(keyPath) { [weak self] observing, change in
+			guard let self = self else { return }
+			
+			let value = self[keyPath: keyPath]
+			self.paraparaView[keyPath: paraparaKeyPath] = value
+		}
+	}
+	func bindParaparaView<Value, RawValue>(_ paraparaKeyPath: WritableKeyPath<ParaparaView, Value>, to keyPath: KeyPath<ViewController, RawValue>) -> Any where Value: RawRepresentable, Value.RawValue == RawValue
+	{
+		return observe(keyPath) { [weak self] observing, change in
+			guard
+				let self = self,
+				let wrappedValue = Value(rawValue: self[keyPath: keyPath])
+				else { return }
+			
+			self.paraparaView[keyPath: paraparaKeyPath] = wrappedValue
+		}
 	}
 }
